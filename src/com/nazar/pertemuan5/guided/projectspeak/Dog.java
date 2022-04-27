4a4f1627895905f9d6f3de58a6702ff11de27771
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2022-04-27 21:12:26 +0700

    Add Inheritance case study

---
+++ src/com/nazar/pertemuan5/guided/projectspeak/Dog.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nazar.pertemuan5.guided.projectspeak;
+
+/**
+ *
+ * @author alwi
+ */
+public class Dog extends Mammal {
+    public void speak() {
+        System.out.println("Arf! Arf!");
+    }
+}
